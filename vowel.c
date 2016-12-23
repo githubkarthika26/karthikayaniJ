ab1096c4b0efb6586d35677aa6e931de1a056ce0
githubkarthika26 <karthikayani.j@gmail.com>
2016-12-23 09:24:52 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int i=0;
+char a[10]={'a','e','i','o','u'},ch;
+printf("get the vowels");
+scanf("%c",&ch);
+if(a[i]==ch)
+{
+printf(" vowel");
+}
+else
+printf(" consonant");
+}
+getch();
+}
