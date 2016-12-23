ee952dc7f5c9f52ee173deffc5db7e719b4093b5
githubkarthika26 <karthikayani.j@gmail.com>
2016-12-23 09:12:36 +0530

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+printf("\n enter the number:");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("\n given number is even");
+}
+else
+{
+printf("\n given number is odd");
+}
+getch();
+}
